6c720adbd4338953ee983abebbdfb00306acad25
Amey <ameyrp3@gmail.com>
2026-06-30 11:51:21 +0530

    Fix: updated environment production API URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-backend-app.onrender.com/api' // तुमच्या क्लाउडवर डिप्लॉय केलेल्या बॅकएंडची लाईव्ह URL येथे टाका
+  apiUrl: 'https://contacts-backend-d0jx.onrender.com/api' // तुमच्या क्लाउडवर डिप्लॉय केलेल्या बॅकएंडची लाईव्ह URL येथे टाका
 };
